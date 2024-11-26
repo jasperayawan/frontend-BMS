2dd81d2a4197181169a8d64c43dd228a3800c8c4
jasperayawan <ejayawan22@gmail.com>
2024-11-26 23:55:48 +0800

    update parse baseURL

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,10 @@ import './App.css'
 import Parse from 'parse/dist/parse.min.js';
 import { Toaster } from 'react-hot-toast';
 import Header from './components/Header';
+import { PARSE } from './helper/api';
 
 Parse.initialize(import.meta.env.VITE_APPLICATION_ID);
-Parse.serverURL = 'http://localhost:8001/parse';
+Parse.serverURL = PARSE;
 
 
 function App() {
