0e356914171f819fe3dd124301c1618f8a39ef8d
jasperayawan <ejayawan22@gmail.com>
2025-03-07 13:48:11 +0800

    update frontend_input set frontend_input = 'text' where frontend_input = 'textarea';

--- src/pages/ResetPassword.jsx
+++ src/pages/ResetPassword.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { useLocation } from 'react-router-dom';
 import { useNewPass } from '../hooks/useNewPass';
+import { set } from 'date-fns';
 
 function useQuery() {
     return new URLSearchParams(useLocation().search);
@@ -10,17 +11,38 @@ const ResetPassword = () => {
     const [newPassword, setNewPassword] = useState('');
     const [confirmPassword, setConfirmPassword] = useState('');
     const { loading, newPass } = useNewPass();
+    const [err, setErr] = useState(null);
     const query = useQuery();
     const token = query.get('token');
 
     const handleSubmit = async (e) => {
         e.preventDefault();
 
+        if(newPassword !== confirmPassword) {
+            setErr('Password does not match');
+            return;
+        }
+
         await newPass(token, newPassword)
     }
 
   return (
-    <div className='min-h-screen flex justify-center items-center flex-col space-y-4 px-4 lg:px-0'>
+    <div className='flex justify-center items-center flex-col space-y-4 px-4 lg:px-0 mt-20'>
+        {err && (
+            <div className="fixed inset-0 bg-black/20 flex justify-center items-center min-h-screen w-full z-50">
+                <div className="px-20 py-7 w-[430px] bg-white border border-orange-600 flex justify-center items-center flex-col gap-y-3">
+                    <h2 className='text-orange-500 text-lg font-semibold text-center'>
+                    {err}
+                    </h2>
+                    <button onClick={() => {
+                    setErr(null);
+                    setNewPassword('');
+                    setConfirmPassword('');
+                    }} className='px-4 py-1 border border-orange-600 text-orange-500 w-[max-content] mx-auto'>OK</button>
+                </div>
+            </div>
+        )}
+
             <h2 className='text-white text-2xl'>Reset Password</h2>
             <div className="mx-auto max-w-md w-full">
                 <form onSubmit={handleSubmit} className='flex flex-col space-y-4'>
@@ -40,7 +62,9 @@ const ResetPassword = () => {
                         className='px-4 py-2 rounded-md border-[1px] border-zinc-300'
                         required
                     />
-                    <button type="submit" className='bg-yellow-600 px-5 py-2 rounded-md'>
+                    <button type="submit" className='w-full py-2 px-4 bg-orange-500 hover:bg-orange-600 border border-transparent rounded-md shadow-sm text-sm font-medium text-white 
+            focus:outline-none focus:ring-2 focus:ring-offset-2
+            disabled:opacity-50 disabled:cursor-not-allowed transition-colors duration-200'>
                         {loading ? 'loading...' : 'Reset Password'}
                     </button>
                 </form>
